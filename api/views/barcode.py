fc2d74d6ed8c9a8731c34e94092e55e528414f16
Wydanhdu <Wydanhdu@gmail.com>
2024-02-11 10:14:02 +0700

    Du

--- api/views/barcode.py
+++ api/views/barcode.py
@@ -11,12 +11,13 @@ def inputNumber(request):
 
     try:
         my_code = EAN13(user_code, writer=ImageWriter())
-        image_path = f"image-barcode/barcode_{user_code}"
+        image_path = f"nextjs/public/assets/images/image-barcode/barcode_{user_code}"
+        image_path1 = f"barcode_{user_code}.png"
         my_code.save(image_path)
     except Exception as e:
         return Response({'error': str(e)}, status=500)
 
-    barcode_data = {'image': image_path}
+    barcode_data = {'image': image_path1}
     serialize = BarcodeSerializer(data=barcode_data)
     
     if serialize.is_valid():
--- nextjs/src/components/Header.tsx
+++ nextjs/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import BoxIcon from "../asset/box.svg"
+import BoxIcon from "../../public/assets/icons/box.svg"
 
 function Header() {
   return (
