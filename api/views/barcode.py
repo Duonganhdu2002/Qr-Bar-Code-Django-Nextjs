4b8874cd6f37ac87ed122da35b7692e6935782db
Wydanhdu <Wydanhdu@gmail.com>
2024-02-17 21:49:11 +0700

    ...

--- api/views/barcode.py
+++ api/views/barcode.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Barcode
 from ..models.barcode import BarcodeSerializer
-from barcode import EAN13
+from barcode import Code128 
 from barcode.writer import ImageWriter
 
 @api_view(['POST'])
@@ -10,7 +10,7 @@ def inputNumber(request):
     user_code = request.data.get('user_code')
 
     try:
-        my_code = EAN13(user_code, writer=ImageWriter())
+        my_code = Code128(user_code, writer=ImageWriter())
         image_path = f"nextjs/public/assets/images/image-barcode/barcode_{user_code}"
         image_path1 = f"barcode_{user_code}.png"
         my_code.save(image_path)
